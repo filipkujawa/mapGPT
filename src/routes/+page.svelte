a32a18d8c19760039e6d066e9139f4e3eaa49fae
Filip Kujawa <filip@filip.local>
2023-02-03 10:41:46 -0800

    improve ui - input & button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,13 @@
 <div class="bg-white min-h-screen">
-    <div class="flex flex-col min-h-screen justify-center items-center">
-        <h1 class="text-6xl font-bold text-black">MapGPT</h1>
+    <div class="flex flex-col min-h-screen items-center pt-24">
+        <h1 class="text-6xl font-bold text-black pb-16">MapGPT</h1>
+        <div class="min-w-full">
         <form method="POST" action="?/submit">
-            <button>Submit</button>
+            <label class="input-group justify-center">
+                <input type="text" placeholder="Type here" class="input input-bordered w-full max-w-xl bg-black" />
+                    <span class="btn btn-info">Submit</span>
+            </label>
         </form>
+        </div>
     </div>
 </div>
