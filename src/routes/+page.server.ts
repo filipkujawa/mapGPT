dfbee0709a242ed8fd0c169ad9ae615341ebdf50
filip <filip.j.kujawa@gmail.com>
2023-02-02 22:42:54 -0800

    make test request to open ai api

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,7 +1,17 @@
 import type { Actions } from './$types';
- 
+import { OPENAI_API_KEY } from '$env/static/private';
+
 export const actions = {
   submit: async (event) => {
     console.log("submit")
+
+    const response = await fetch('https://api.openai.com/v1/models', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + OPENAI_API_KEY
+        }
+    }).then((response) => response.json())
+    console.log(response)
   }
 } satisfies Actions;
