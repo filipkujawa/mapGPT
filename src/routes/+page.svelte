e502fd41f54267735e3de9902091f6cb0d4cd5f4
filip <filip.j.kujawa@gmail.com>
2023-02-03 23:13:55 -0800

    add basic graph visualization

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,28 @@
+<script>
+    import { DataSet, Network } from 'vis';
+    import { onMount } from 'svelte';
+
+    let network;
+
+    onMount(async () => {
+        const nodes = new DataSet([
+            { id: 1, label: 'Node 1', color:'#3ABFF8'},
+            { id: 2, label: 'Node 2', color:'#3ABFF8' },
+            { id: 3, label: 'Node 3',color:'#3ABFF8' }
+        ]);
+        
+        const edges = new DataSet([
+            { from: 1, to: 2, color: {color:'black'} },
+            { from: 2, to: 3, color: {color:'black'} }
+        ]);
+
+
+        
+        const container = document.getElementById('myGraph');
+        network = new Network(container, { nodes, edges }, {});
+    });
+</script>
+
 <div class="bg-white min-h-screen">
     <div class="flex flex-col min-h-screen items-center pt-24">
         <h1 class="text-6xl font-bold text-black pb-16">MapGPT</h1>
@@ -8,6 +33,9 @@
                     <span class="btn btn-info">Submit</span>
             </label>
         </form>
+        </div>
+        <div class="min-w-full min-h-screen flex justify-center">
+            <div id="myGraph"></div>
         </div>
     </div>
 </div>
