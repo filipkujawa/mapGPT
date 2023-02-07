46063eb6b3976036cd49f5e47fb786b3e73107f7
filip <filip.j.kujawa@gmail.com>
2023-02-07 00:11:43 -0800

    prompt fixes

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -1,22 +1,18 @@
 export const PROMPT_BASE = `
+Create a graph representation of relationships between concepts in a given input. Each node in the graph should represent a concept with an id, a label property, and a color property (in hex code format). The hex code should represent the node's concept or if unable to assign the concept a color set color to #3ABFF8.The relationship between two nodes is represented as an edge with a to and from property that corresponds to the ids of the two nodes, and a label property. The output should be formatted as a valid JSON object, similar to the example below.
+
 Example:
 "Shrek is friends with Donkey"
 
 {
-    "nodes": [
-        { "id": 1, "label": "Shrek", "color": "#7fcc6e"},
-        { "id": 2, "label": "Donkey", "color": "#63543e"}
-    ],
-    "edges": [
-        { "from": 1, "to": 2,  "label": "Friends"}
-    ]
+"nodes": [
+{ "id": 1, "label": "Shrek", "color": "#7fcc6e"},
+{ "id": 2, "label": "Donkey", "color": "#63543e"}
+],
+"edges": [
+{ "from": 1, "to": 2, "label": "Friends"}
+]
 }
 
-Task: Given an input, extrapolate as many relationships between concepts as possible from the input. 
-Every node, a concept, has an id, a label property, and a color property (Hex Code).
-If possible, the hex code should represent the node or just be #3ABFF8.
-Every edge, a relationship between two nodes, has a to-and-from property corresponding to the ids of the two nodes and a label property. 
-The output should be formatted like the example output above, in valid json so it can be parsed.
-
-Input: 
+Input:
 `;
